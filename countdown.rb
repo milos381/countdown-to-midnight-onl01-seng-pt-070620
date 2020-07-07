dd98c377f58b56ed5b9d4e750383a3714fa6fbcb
Milos Popovic <milos_popovic@outlook.com>
2020-07-07 17:48:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,9 @@ def countdown(number)
   while number > 0
     number -= 1
     puts "#{number} SECOND(S)!"
-    if number == 0
-      puts "HAPPY NEW YEAR!"
-    end
-  end
 
+  end
+  if number == 0
+    puts "HAPPY NEW YEAR!"
+  end
 end
