4d8c0d0117c2d52a7e4169f2078a30225e078f26
Milos Popovic <milos_popovic@outlook.com>
2020-07-07 17:57:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,14 @@ def countdown(number)
    "HAPPY NEW YEAR!"
 
 end
+def countdown_with_sleep(number)
+  while number > 0
+
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+  end
+
+   "HAPPY NEW YEAR!"
+
+end
